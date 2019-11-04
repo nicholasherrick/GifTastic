a409a710f795c3682f9e040c28a8cc921baeb5ee
Nick Herrick <nth.herrick@gmail.com>
2019-11-03 21:17:00 -0500

    played around with the javascript code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,126 @@
 $(document).ready(function() {
 
+var topics = ["tacos", "burgers", "pizza", "salad", "pasta", "candy", "cake", "sandwiches", "sushi", "burritos", "chicken wings", "ramen"];
+var food = $(this).attr("data-name");
+var apiKey = "rQ6AHv1YvrXWOwMEJYDSe60UnMh4AgIB";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=" + apiKey + "&limit=10";
 
+for (var i = 0; i < topics.length; i++) {
+    newButton.addClass("new-button");
+    newButton.attr("data-name", topics[i]);
+    newButton.text(topics[i]);
+    buttonsDiv.append(newButton);
+}
 
+function test() {
+$.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
 
-
-
-
+};
+test();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+
+
+// var topics = ["tacos", "burgers", "pizza", "salad", "pasta", "candy", "cake", "sandwiches", "sushi", "burritos", "chicken wings", "ramen"];
+// var buttonsDiv = $("#buttons-div");
+
+// renderButtons();
+
+// function renderButtons() {
+    
+//     var newButton = $("<button type='button' class='btn btn-secondary btn-lg'>");
+//     $("#buttons-div").empty();
+//     for (var i = 0; i < topics.length; i++) {
+//         newButton.addClass("new-button");
+//         newButton.attr("data-name", topics[i]);
+//         newButton.text(topics[i]);
+//         buttonsDiv.append(newButton);
+//     }
+
+// };
+
+// // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + addNew + "&api_key=" + apiKey + "&limit=10";
+// // var apiKey = "rQ6AHv1YvrXWOwMEJYDSe60UnMh4AgIB";
+
+// var addNew = $("#food-input").val().trim();
+// // var topics = ["tacos", "burgers", "pizza", "salad", "pasta", "candy", "cake", "sandwiches", "sushi", "burritos", "chicken wings", "ramen"];
+
+// function showFood() {
+//     var food = $(this).attr("data-name");
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=" + apiKey + "&limit=10";
+//     var apiKey = "rQ6AHv1YvrXWOwMEJYDSe60UnMh4AgIB";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         var rating = response;
+//         $("#gif-box").append();
+//     });
+// };
+
+// $(document).on("click", ".new-Button", showFood);
+
+
+// // var newButton = $("<button type='button' class='btn btn-secondary btn-lg new-Button'>");
+
+
+// $("#add-food").on("click", function (event) {
+//     event.preventDefault();
+//     var food = $("#food-input").val().trim();
+//     topics.push(food);
+//     renderButtons();
+// });
+
+// });
