aa6a0e41a9032ff8bd86ba05e018e6f6864b3f1b
Nick Herrick <nth.herrick@gmail.com>
2019-11-03 22:50:23 -0500

    added working functionality for clicking each button to display 10 gifs with a rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@ var buttonsDiv = $("#buttons-div");
 
 
 function showButtons() {
+    buttonsDiv.empty();
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<Button type='button' class='btn btn-secondary btn-lg'>")
         newButton.addClass("new-button");
-        newButton.attr("data-name", topics[i]);
+        newButton.attr("data-name", topics[i].split(" ").join("_"));
         newButton.text(topics[i]);
         buttonsDiv.append(newButton);
     }
@@ -24,113 +25,30 @@ function getFood() {
             method: "GET"
         }).then(function(response) {
             console.log(response);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var newDiv = $("<div>");
+                var gifBox = $("#gif-box");
+                var text = $("<p>").text("Rating: " + rating);
+                var image = $("<img src=" + gif + ">");
+                var rating = results[i].rating;
+                var gif = results[i].images.downsized.url;
+                newDiv.append(text);
+                newDiv.append(image);
+                gifBox.prepend(newDiv);
+            }
         });
 
 };
 
-$(document).on("click", ".new-button", getFood);
-
-// test();
-showButtons();
+$("#add-food").on("click", function (event) {
+    event.preventDefault();
+    var food = $("#food-input").val().trim();
+    topics.push(food);
+    showButtons();
 });
 
+$(document).on("click", ".new-button", getFood);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-
-
-// var topics = ["tacos", "burgers", "pizza", "salad", "pasta", "candy", "cake", "sandwiches", "sushi", "burritos", "chicken wings", "ramen"];
-// var buttonsDiv = $("#buttons-div");
-
-// renderButtons();
-
-// function renderButtons() {
-    
-//     var newButton = $("<button type='button' class='btn btn-secondary btn-lg'>");
-//     $("#buttons-div").empty();
-//     for (var i = 0; i < topics.length; i++) {
-//         newButton.addClass("new-button");
-//         newButton.attr("data-name", topics[i]);
-//         newButton.text(topics[i]);
-//         buttonsDiv.append(newButton);
-//     }
-
-// };
-
-// // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + addNew + "&api_key=" + apiKey + "&limit=10";
-// // var apiKey = "rQ6AHv1YvrXWOwMEJYDSe60UnMh4AgIB";
-
-// var addNew = $("#food-input").val().trim();
-// // var topics = ["tacos", "burgers", "pizza", "salad", "pasta", "candy", "cake", "sandwiches", "sushi", "burritos", "chicken wings", "ramen"];
-
-// function showFood() {
-//     var food = $(this).attr("data-name");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=" + apiKey + "&limit=10";
-//     var apiKey = "rQ6AHv1YvrXWOwMEJYDSe60UnMh4AgIB";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         console.log(response);
-//         var rating = response;
-//         $("#gif-box").append();
-//     });
-// };
-
-// $(document).on("click", ".new-Button", showFood);
-
-
-// // var newButton = $("<button type='button' class='btn btn-secondary btn-lg new-Button'>");
-
-
-// $("#add-food").on("click", function (event) {
-//     event.preventDefault();
-//     var food = $("#food-input").val().trim();
-//     topics.push(food);
-//     renderButtons();
-// });
-
-// });
+showButtons();
+});
